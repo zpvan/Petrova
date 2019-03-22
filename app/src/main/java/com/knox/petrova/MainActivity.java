914e31dbe0b28526df28ba8afccaed97c27dc5c1
zpvan <843762968@qq.com>
2019-03-22 10:46:37 +0800

    start activity which is in an android library

--- app/src/main/java/com/knox/petrova/MainActivity.java
+++ app/src/main/java/com/knox/petrova/MainActivity.java
@@ -1,7 +1,10 @@
 package com.knox.petrova;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.knox.kismet.NativeWrapper;
@@ -15,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tvText = findViewById(R.id.tv_test);
         tvText.setText(NativeWrapper.stringFromJNI());
+
+        tvText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAarActivity("com.knox.petrova", "com.knox.kismet.KismetActivity");
+            }
+        });
+    }
+
+    /**
+     * 启动存在于android library的activity
+     */
+    private void startAarActivity(String myPackageName, String targetActivityName) {
+        Intent intent = new Intent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setComponent(new ComponentName(myPackageName, targetActivityName));
+        getApplicationContext().startActivity(intent);
     }
 }
---
+++ kismet/src/main/java/com/knox/kismet/KismetActivity.java
@@ -0,0 +1,13 @@
+package com.knox.kismet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KismetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kismet);
+    }
+}
