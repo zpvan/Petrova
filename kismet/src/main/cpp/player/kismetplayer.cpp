c514f52f5668192eaf3b251cadbc08bfef95126a
knox <843762968@qq.com>
2019-03-27 11:38:35 +0800

    plan the code flow of kismetplayer, and add SurfaceView in a demo activity

--- kismet/src/main/cpp/player/kismetplayer.cpp
+++ kismet/src/main/cpp/player/kismetplayer.cpp
@@ -10,6 +10,14 @@
 
 void kismetplayer::setDataSource (const char *path) {
     KLOGE(TAG_LOG, "setDataSource %s", path);
+#if 0
+    // 注册所有的文件格式和编解码器的库
+    av_register_all();
+
+    // 打开多媒体文件
+    AVFormatContext *pFormatCtx = NULL;
+    avformat_open_input(&pFormatCtx, url, NULL, NULL);
+#endif
 }
 
 void kismetplayer::setDisplay (void *surface) {
@@ -18,6 +26,36 @@ void kismetplayer::setDisplay (void *surface) {
 
 void kismetplayer::prepare () {
     KLOGE(TAG_LOG, "prepare");
+#if 0
+    // 解析流讯息, 耗时
+    avformat_find_stream_info(pFormatCtx, NULL);
+
+    // 找到视频流
+    int videoStream = -1;
+    int i;
+    for (i = 0; i < pFormatCtx->nb_streams; i++)
+    {
+        if (pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            videoStream = i;
+            break;
+        }
+    }
+
+    // 获取视频编解码器上下文
+    AVCodecContext *pCodecCtx = NULL;
+    pCodecCtx = pFormatCtx->streams[videoStream]->codec;
+
+    // 找到对应的视频解码器
+    AVCodec *pCodec = NULL;
+    pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
+
+    // 打开解码器
+    avcodec_open2(pCodecCtx, pCodec, NULL);
+
+    // 分配视频帧内存空间
+    pFrame = av_frame_alloc();
+#endif
 }
 
 void kismetplayer::prepareAsync () {
@@ -26,10 +64,26 @@ void kismetplayer::prepareAsync () {
 
 void kismetplayer::start () {
     KLOGE(TAG_LOG, "start");
+#if 0
+    // 解码视频帧
+    if (packet.stream_index == videoStream)
+    {
+        int frameFinished;
+        avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, &packet);
+        if (frameFinished)
+        {
+            saveIt()
+        }
+    }
+#endif
 }
 
 void kismetplayer::stop () {
     KLOGE(TAG_LOG, "stop");
+#if 0
+    // 释放packet, 它是在av_read_frame里边分配内存packet.data
+    av_free_packet(&packet);
+#endif
 }
 
 void kismetplayer::pause () {
@@ -42,4 +96,7 @@ void kismetplayer::reset () {
 
 void kismetplayer::release () {
     KLOGE(TAG_LOG, "release");
+#if 0
+    releaseResource();
+#endif
 }
--- kismet/src/main/java/com/knox/kismet/KismetActivity.java
+++ kismet/src/main/java/com/knox/kismet/KismetActivity.java
@@ -1,11 +1,20 @@
 package com.knox.kismet;
 
+import android.graphics.Color;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class KismetActivity extends AppCompatActivity {
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class KismetActivity extends AppCompatActivity implements SurfaceHolder.Callback{
 
     private static final String TAG = "KismetActivity";
     private PlayerProxy mPlayer;
@@ -13,16 +22,16 @@ public class KismetActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kismet);
+
+        setUpView();
+        // setContentView(R.layout.activity_kismet);
     }
 
     @Override
     protected void onStart() {
         Log.e(TAG, "onStart: ");
         super.onStart();
-        mPlayer = new PlayerProxy();
-        mPlayer.setDataSource(Environment.getExternalStorageDirectory().getPath() + "/Download/valor-01-01.mp4");
-        mPlayer.prepare();
+
     }
 
     @Override
@@ -51,5 +60,55 @@ public class KismetActivity extends AppCompatActivity {
         Log.e(TAG, "onDestroy: ");
         super.onDestroy();
         mPlayer.release();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.e(TAG, "surfaceCreated: HolderCallback");
+        mPlayer = new PlayerProxy();
+        mPlayer.setDataSource(Environment.getExternalStorageDirectory().getPath() + "/Download/valor-01-01.mp4");
+        mPlayer.setDisplay(holder);
+        mPlayer.prepare();
+        mPlayer.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Log.e(TAG, "surfaceChanged: HolderCallback");
+        mPlayer.setDisplay(holder);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.e(TAG, "surfaceDestroyed: HolderCallback");
+        mPlayer.release();
+    }
+
+    private void setUpView() {
+        FrameLayout frameLayout = new FrameLayout(this);
+        GLSurfaceView glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.getHolder().addCallback(this);
+        glSurfaceView.setRenderer(new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+                Log.e(TAG, "onSurfaceCreated: GLSurfaceView.Renderer");
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl, int width, int height) {
+                Log.e(TAG, "onSurfaceChanged: GLSurfaceView.Renderer");
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl) {
+                GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            }
+        });
+        TextView textView = new TextView(this);
+        textView.setText(TAG);
+        textView.setTextColor(Color.RED);
+        frameLayout.addView(glSurfaceView);
+        frameLayout.addView(textView);
+        setContentView(frameLayout);
     }
 }
